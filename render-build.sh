c1aef511244afc670bec387980b71103a6388c41
Shreya kumari <shreyakumari4894@email.com>
2026-05-02 20:17:01 +0530

    Fixed dependency conflicts and cleaned requirements

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Dependencies install karein
 pip install -r requirements.txt
 
+# Static files collect karein (Healthcare+ design ke liye)
 python manage.py collectstatic --no-input
+
+# Database tables banayein
 python manage.py migrate
+
+# Admin user banayein (Aapka custom script)
 python create_admin.py
