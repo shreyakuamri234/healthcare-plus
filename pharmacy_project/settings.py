19d916729d2c55ac959d9aa19de4377e7b6f83b1
Shreya kumari <shreyakumari4894@email.com>
2026-05-02 19:20:34 +0530

    Static files fix for Healthcare+

--- pharmacy_project/settings.py
+++ pharmacy_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Ye line SecurityMiddleware ke theek niche honi chahiye
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'pharmacy_project.urls'
 
 
