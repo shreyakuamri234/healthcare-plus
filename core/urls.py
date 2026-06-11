1a2cd23566d8f493b8fdbe4a88a113256156f328
Shreya kumari <shreyakumari4894@email.com>
2026-06-11 22:06:08 +0530

    Updated success page and linked to book_appointment

--- core/urls.py
+++ core/urls.py
@@ -29,9 +29,9 @@ urlpatterns = [
     # --- Lab Tests & Appointments ---
     path('lab-test/', views.add_lab_test, name='lab_test'),
     path('place-lab-order/', views.place_lab_order, name='place_lab_order'),
-    path('booking-success/<int:appointment_id>/', views.booking_success, name='booking_success'),
+    path('book-appointment/', views.book_appointment, name='book_appointment'),
     path('membership/', views.buy_membership, name='membership'),
     path('health/', views.health, name='health'),
-    path('map/', views.map, name='map'),
+    
 
 ]
